dac88251693c5628549eef5d99a063a4ac1f61a7
jb <jb.williams016@gmail.com>
2024-02-06 21:25:39 -0600

    removed doonce random function, not sure if its necessary

--- main.go
+++ main.go
@@ -27,9 +27,10 @@ var d100 = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 
 func rollDice(value []int, numTimes *int) {
 	var myRoll int
 	for num := *numTimes; num >= 1; num-- {
-		DoOnce.Do(func() {
-			rand.Seed(time.Now().UnixNano())
-		})
+		//DoOnce.Do(func() {
+		//rand.Seed(time.Now().UnixNano())
+		//})
+		rand.Seed(time.Now().UnixNano())
 		myRoll = value[rand.Intn(len(value))]
 		fmt.Printf("%d\n", myRoll)
 	}
